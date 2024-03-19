d4e4060d799f74fc54578215c6a02e87105888d2
Jonatha Targino <j.oliveiratargino@hotmail.com>
2024-03-19 11:47:12 -0300

    structure(MB002): install & config eslint, editorconfig and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import 'dotenv/config'
-import { AppDataSource } from "./data-source"
-import * as express from "express"
+import "dotenv/config";
+import { AppDataSource } from "./data-source";
+import * as express from "express";
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Worlde!")
-})
+  res.send("aaa");
+});
 
-AppDataSource.initialize().then(async () => {
-    app.listen(process.env.APP_PORT, () => console.log(`Server running at ${process.env.APP_PORT}`))
-}).catch(error => console.log(error))
-
+AppDataSource.initialize()
+  .then(async () => {
+    app.listen(process.env.APP_PORT, () => {
+      console.log(`Server running at ${process.env.APP_PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
