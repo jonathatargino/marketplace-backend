f5db9a88272529f0a748984723f13226492a5eda
Jonatha Targino <j.oliveiratargino@hotmail.com>
2024-03-19 11:18:32 -0300

    structure(MB002): organize folder structure, install nodemon, create dev script, remove typeorm default entities

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,14 @@
 import 'dotenv/config'
-
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
 import * as express from "express"
 
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello Worlde!")
+})
+
 AppDataSource.initialize().then(async () => {
-
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    const app = express()
-
-    app.get("/", (req, res) => {
-        res.send("Hello World!")
-    })
-
-    app.get("/users", async (req, res) => {
-        const users = await AppDataSource.manager.find(User)
-        res.json(users)
-    })
-
     app.listen(process.env.APP_PORT, () => console.log(`Server running at ${process.env.APP_PORT}`))
-
 }).catch(error => console.log(error))
 
